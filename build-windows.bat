42869f9d12ceeb5496e5447ded4a2701d34c214d
DennyKuo <45343949+DennyKuo0809@users.noreply.github.com>
2024-01-04 17:21:48 +0800

    Fix: pip install failed

--- build-windows.bat
+++ build-windows.bat
@@ -1,3 +1,4 @@
-python3 -m pip install requirements.txt
-pyinstaller -y --clean --name stock.exe --hidden-import=Import.txt api_server.py --onefile
+pip install -r requirements.txt
+pip install pyinstaller
+pyinstaller -y --clean --name stock.exe --hidden-import=requirements.txt api_server.py --onefile
 copy dist\stock.exe .\stock.exe
