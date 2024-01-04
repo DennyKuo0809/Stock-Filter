c1d16504461f9afd17560d51e0ea93d97333c806
b07902067 <b07902067@ntu.edu.tw>
2024-01-04 15:34:10 +0800

    Modify: windows build script

--- build-windows.bat
+++ build-windows.bat
@@ -1,3 +1,3 @@
 python3 -m pip install requirements.txt
 pyinstaller -y --clean --name stock.exe --hidden-import=Import.txt api_server.py --onefile
-copy dist\stock.exe ./
+copy dist\stock.exe .\stock.exe
